102e1a82738880d5a1b17430031572f0a45e4c98
Bryan Shelton <bryan@sheltonplace.com>
2013-04-08 07:55:11 -0500

    Need to fix up the resumé, put pjax in a selector.

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,7 +3,7 @@ hljs.initHighlightingOnLoad();
 
 $(function() {
   // Pjaxify the site
-  $(document).pjax('a', '#content', { fragment: '#content' });
+  $(document).pjax('a', '#bryan', { fragment: '#content' });
   $(document).on('pjax:end', function() {
     $('pre code').each(function(i, e) {
       hljs.highlightBlock(e);
