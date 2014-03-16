a91ca70ff86914816343dcf4563b11ec50de350c
Bryan Shelton <bryan@sheltonplace.com>
2014-03-15 18:47:35 -0700

    Site v3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,44 +5,42 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
-    less: {
-      assets: {
-        options: {
-          yuicompress: true
-        },
+    sass: {
+      app: {
         files: {
-          'assets/css/app.css': '_assets/less/app.less'
+          'assets/css/app.css': 'assets/sass/all.scss'
+        }
+      }
+    },
+    cssmin: {
+      app: {
+        files: {
+          'assets/css/app.min.css': 'assets/css/app.css'
         }
       }
     },
     uglify: {
-      assets: {
+      options: {
+        preserveComments: 'some'
+      },
+      app: {
         files: {
-          'assets/js/app.js': [
-            '_assets/js/bootstrap.js'
-          ]
+          'assets/js/app.js': ['assets/js/src/vendor/**/*.js', 'assets/js/src/app.js']
         }
       }
     },
     watch: {
-      js: {
-        files: [ '_assets/js/**/*.js' ],
-        tasks: [ 'uglify' ]
-      },
-      less: {
-        files: [ '_assets/less/**/*.less' ],
-        tasks: [ 'less' ]
-      }
+      sass: { files: 'assets/sass/**/*.scss', tasks: ['sass', 'cssmin'] },
+      js: { files: 'assets/js/src/**/*.js', tasks: ['uglify']}
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task.
-  grunt.registerTask('default', ['less', 'uglify']);
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
 };
---
+++ assets/js/src/app.js
@@ -0,0 +1,44 @@
+$(function() {
+  /*!Email obfuscator script 2.1 by Tim Williams, University of Arizona
+  Random encryption key feature by Andrew Moulden, Site Engineering Ltd
+  This code is freeware provided these four comment lines remain intact
+  A wizard to generate this code is at http://www.jottings.com/obfuscator/*/
+  var email_address = function() {
+    var coded = "R2A3i@xtVL6Gi0L3dV.dGS",
+        key = "aAErY7CUi1OPmhdsolf5He3XF24pT9kuIWStbxGLQyBVMZgwn0vJDqcj86RKzN",
+        shift=coded.length,
+        link="";
+
+    for (i=0; i<coded.length; i++) {
+      if (key.indexOf(coded.charAt(i))==-1) {
+        ltr = coded.charAt(i)
+        link += (ltr)
+      }
+      else {
+        ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+        link += (key.charAt(ltr))
+      }
+    }
+
+    return link;
+  }
+
+  $(".myemail").attr('href', 'mailto:'+email_address());
+
+  // https://github.com/markdalgleish/stellar.js
+  if(!Modernizr.touch){
+    $.stellar();
+  }
+
+  // http://calebjacob.com/tooltipster
+  $('.tooltipster').tooltipster();
+
+  // smooth scrolling on links
+  var $root = $('html, body');
+  $('a').click(function () {
+    $root.delay(300).animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
+});
